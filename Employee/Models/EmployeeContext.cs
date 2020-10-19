35f290ec0470f3f1379ddb6e1adf76e37b9ef193
Ricki Irawan <ricki.irawan.ft18@mail.umy.ac.id>
2020-10-19 19:46:52 +0700

    Add project files.

---
+++ Employee/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+       base(options)
+        { }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
